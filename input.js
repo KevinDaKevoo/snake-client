d5a21666e0ed12f5ae9ee721f2b46ee98292c61f
Kevin Yu <Kevin.yu.kfy@gmail.com>
2021-06-02 18:56:08 +0000

    Updated input.js

--- input.js
+++ input.js
@@ -1,21 +1,35 @@
-const setupInput = function () {
+let connection;
+
+const w = "Move: up";
+const s = "Move: down";
+const a = "Move: left";
+const d = "Move: right";
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  const handleUserInput = (key) => {
+    process.stdout.write('Input: ');
+    if (key === '\u0003') {
+      process.exit();
+    } if (key === 'w') {
+      conn.write(w);
+    } if (key === 's') {
+      conn.write(s);
+    } if (key === 'a') {
+      conn.write(a);
+    } if (key === 'd') {
+      conn.write(d);
+    }
+  };
   stdin.on("data", handleUserInput);
 
   return stdin;
 };
 
-
-const handleUserInput = (key) => {
-  process.stdout.write('.');
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-setupInput(handleUserInput)
 
 module.exports = { setupInput }
